5fa42444303f623b4b4991e4652de7a942df1e07
Lyndon <dragoneer57@outlook.com>
2021-04-29 11:01:03 -0500

    Suffering :)

--- hack.hs
+++ hack.hs
@@ -1,8 +1,21 @@
-{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, PartialTypeSignatures #-}
 
 import Database.SQLite.Simple
-import Data.Text.Lazy (Text, pack, unpack)
+--import Data.Text.Lazy (Text, pack, unpack)
 import Data.Int
+import Data.Text
+import qualified Database.SQLite3 as SQLite3
+import Control.Exception
+import Control.Concurrent
+
+import Control.Applicative
+import Data.Maybe (isJust, catMaybes)
+import Data.List (takeWhile)
+import Control.Monad (forever, forM, forM_, unless)
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TChan
+import System.IO.Unsafe
+import Data.Function(fix)
 
 hello = do
   conn <- open "test.db"
@@ -45,7 +58,8 @@ runPersonExample = do
  -- executeNamed conn "UPDATE people SET parameter = value WHERE parameter = :parameter AND value = :value" [":parameter" := ("age" :: String), ":value" := ("3" :: String)]
 
   --executeNamed conn "INSERT INTO people (name, parameter) VALUES (Joe, value) WHERE parameter = :parameter AND value = :value" [":parameter" := ("age" :: String), ":value" := ("3" :: String)]
-  executeNamed conn "INSERT INTO people (name, parameter) VALUES (Joe, value)" ["parameter" := ("age" :: String), "value" := ("3" :: String)]
+  --executeNamed conn "INSERT INTO people (name, parameter) VALUES (Joe, value)" ["parameter" := ("age" :: String), "value" := ("3" :: String)]
+  execute conn "INSERT INTO people (?) VALUES (3)" (Only "age":: Only String)
   --executeNamed conn "INSERT INTO people (param1, param2) VALUES (val1, val2) WHERE param1 = :param1 AND param2 = :param2 AND val1 = :val1 AND val2 = :val2" [":param1" := ("name" :: String),":param2" := ("age" :: String),":val1" := ("Joe" :: String), ":val2" := ("3" :: String)]
   close conn
   print people
@@ -89,3 +103,71 @@ error:
 -- https://www.sqlite.org/lang_insert.html
 
 -- http://hackage.haskell.org/package/postgresql-simple-named-0.0.0.0/docs/PgNamed.html
+
+
+-- Attempt to use lowest level bindings.
+
+specialExample::IO()
+specialExample = do
+    conn <- SQLite3.open "test4.db"
+    --SQLite3.exec conn $ sqlite3_set_prototype "people" "age" "3"
+    querydata <- sqlite3_get_prototype conn "people" "age"
+    SQLite3.close conn
+    print querydata
+
+sqlite3_set_prototype :: String -> String -> String -> Text
+sqlite3_set_prototype mytable parameters item = pack $ "INSERT INTO "++ mytable ++" ("++ parameters ++") VALUES ("++ item ++")"
+
+sqlite3_get_prototype :: SQLite3.Database -> String -> String -> IO [(SQLite3.ColumnCount, [Text], [Maybe Text])] -- _
+sqlite3_get_prototype conn mytable parameters = do
+    let myquery = pack $ "SELECT " ++ parameters ++ " from " ++ mytable
+    --SQLite3.execPrint conn myquery
+
+    --let myfunction =  bracket (SQLite3.prepare conn myquery) SQLite3.finalize
+    --myfunction SQLite3.columns
+
+    --chan <- newTChan
+    --let exec' sql = SQLite3.execWithCallback conn sql $ \c n v -> atomically $ writeTChan chan (c, n, v)
+
+    -- let chan = (3::SQLite3.ColumnCount, ["Base"]::[Text], [(Just "Base")]::[Maybe Text])
+    -- tha <- newTVarIO chan 
+    -- let exec' sql = SQLite3.execWithCallback conn sql $ \c n v -> atomically $ writeTVar tha (c, n, v)
+    -- exec' myquery
+
+    chan2 <- newTChanIO -- (TChan(SQLite3.ColumnCount,[Text],[Maybe Text]))
+    let exec'' sql = SQLite3.execWithCallback conn sql $ \c n v -> atomically $ writeTChan chan2 (c, n, v)
+    exec'' myquery
+    --stmt <- SQLite3.prepare conn myquery
+    --SQLite3.Row <- SQLite3.step stmt
+   -- res <- SQLite3.column stmt 0
+    --SQLite3.Done <- SQLite3.step stmt
+    --SQLite3.finalize stmt
+    --return res
+    let mylist = []
+    --let makelist object = object : mylist
+    --untilEmpty chan2 makelist
+    --let extractJust = Data.List.takeWhile unless . isEmptyTChan
+    --ch' <- fmap extractJust $ atomically $ readTChan chan2
+    --forM_ ch' $ \x -> putStr (show x ++ ",")
+    --untilEmpty chan2 print
+    myempty chan2 mylist
+    --let links = catMaybes $ Data.List.takeWhile isJust results
+    --return links
+
+myempty :: TChan a -> [a] -> IO [a]
+myempty chan mylist = do
+    empty <- atomically (isEmptyTChan chan)
+    case empty of
+        (True) -> pure(mylist)
+        (False) -> do
+            value <- atomically (readTChan chan)
+            myempty chan (value : mylist)
+
+
+untilEmpty :: TChan t -> (t -> IO a) -> IO ()
+untilEmpty chan action = fix $ \next -> do
+    empty <- atomically (isEmptyTChan chan)
+    unless empty $ do
+        value <- atomically (readTChan chan)
+        action value
+        next
